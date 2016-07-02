d10b5918d9bce8f31d8a0b3405312bd32cddf76f
Alexander K. Hudek <alexander@hudek.org>
2016-07-01 20:06:11 -0400

    Add second namespace.

--- src/ui_test/table.cljs
+++ src/ui_test/table.cljs
@@ -1,31 +1,29 @@
-(ns ui-test.core
+(ns ui-test.table
   (:require
-    [reagent.core :as r]
-    [sablono.core :as sab :include-macros true]
-    ui-test.table)
+    [reagent.core :as r])
   (:require-macros
     [devcards.core :as dc :refer [defcard deftest]]))
 
 (enable-console-print!)
 
 #_(defn simple-input []
-  (let [text (r/atom "")]
-    (fn []
-      [:input {:type "text"
-               :value @text
-               :on-change #(reset! text (-> % .-target .-value))}])))
+    (let [text (r/atom "")]
+      (fn []
+        [:input {:type "text"
+                 :value @text
+                 :on-change #(reset! text (-> % .-target .-value))}])))
 
 (defn simple-input
   [initial-value opts]
   (let [s {:text (r/atom initial-value)}]
     {:state s
      :render
-     (fn []
-      [:input {:class (or (:class opts) "")
-               :type (or (:type opts) "text")
-               :on-blur #(when-let [f (:on-blur opts)] (f % s))
-               :value @(:text s)
-               :on-change #(reset! (:text s) (-> % .-target .-value))}])}))
+            (fn []
+              [:input {:class (or (:class opts) "")
+                       :type (or (:type opts) "text")
+                       :on-blur #(when-let [f (:on-blur opts)] (f % s))
+                       :value @(:text s)
+                       :on-change #(reset! (:text s) (-> % .-target .-value))}])}))
 
 (defn input-with-icon
   [icon opts]
