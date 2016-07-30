7df280f1b313089379482784ca57ad594b1525b0
Alexander Hudek <alex@kirasystems.com>
2016-07-30 16:08:26 -0400

    Updated so that each tooltip has it's own base element and added position top middle.

--- src/ui_test/tooltip.cljs
+++ src/ui_test/tooltip.cljs
@@ -1,6 +1,6 @@
 (ns ui-test.tooltip
   (:require
-    [dommy.core :as dom]
+    [dommy.core :as dom :refer-macros [sel1]]
     [reagent.core :as r]
     [sablono.core :as sab :include-macros true])
   (:require-macros
@@ -22,80 +22,89 @@
 ;; destruction.
 
 (def initial-state {:top -1000 :left -1000 :msg "" :positioned false :visible false})
-(def tooltip-state (r/atom initial-state))
-(def tooltip-timer (r/atom nil))
 (def tooltip-delay 500)
 (def tooltip-hide-delay 500) ;; needs to match css
-(def tooltip-hide-timer (r/atom nil))
+(def default-opts {:position :top})
 
 (defn tooltip-base
   "The base tooltip component shared by all tooltips. This should
   be mounted as close to the body as possible."
-  []
+  [base-id tooltip-state message]
   (when (and (:positioned @tooltip-state) (not (:visible @tooltip-state)))
     (swap! tooltip-state assoc :visible true))
-  [:div.tooltip {:style {:position "fixed"
+  [:div.tooltip {:id base-id
+                 :style {:position "fixed"
                          :top (:top @tooltip-state)
                          :left (:left @tooltip-state)
                          :background "black"
                          :color "white"}
                  :class (if (:visible @tooltip-state) "show" "")}
-   (:msg @tooltip-state)])
+   message])
 
 (defn start-timer!
-  [f]
+  [tooltip-timer tooltip-hide-timer f]
   (reset! tooltip-timer (.setTimeout js/window f tooltip-delay))
   (when-let [id @tooltip-hide-timer]
     (.clearTimeout js/window id)))
 
 (defn start-hide-timer!
-  []
+  [tooltip-state tooltip-hide-timer]
   (reset! tooltip-hide-timer (.setTimeout js/window #(reset! tooltip-state initial-state) tooltip-hide-delay)))
 
 (defn stop-timer!
-  []
+  [tooltip-timer]
   (.clearTimeout js/window @tooltip-timer))
 
 (defn set-position!
   "Set the position of the tooltip based on the supplied element."
-  [el]
-  (let [rect (dom/bounding-client-rect el)]
-    (swap! tooltip-state assoc :top (- (:top rect) 20) :left (:left rect) :positioned true)))
+  [tooltip-state opts base-id el]
+  (let [tgt-rect (dom/bounding-client-rect el)
+        tip-rect (dom/bounding-client-rect (sel1 (str "#" base-id)))]
+    (case (:position opts)
+      :top
+      (swap! tooltip-state assoc
+             :top (- (:top tgt-rect) (:height tip-rect))
+             :left (- (+ (:left tgt-rect) (/ (:width tgt-rect) 2.0)) (/ (:width tip-rect) 2.0))
+             :positioned true))))
 
 (defn reset-tooltip!
   "Reset the tooltip to initial conditions."
-  []
-  (stop-timer!)
+  [tooltip-timer tooltip-hide-timer tooltip-state]
+  (stop-timer! tooltip-timer)
   (swap! tooltip-state assoc :visible false :positioned false)
-  (start-hide-timer!))
+  (start-hide-timer! tooltip-state tooltip-hide-timer))
 
 (defn with-tooltip
-  [message]
-  (let [node (r/atom nil)]
+  [message opts]
+  (let [node          (r/atom nil)
+        tooltip-state (r/atom initial-state)
+        tooltip-timer (r/atom nil)
+        tooltip-hide-timer (r/atom nil)
+        base-id       (str "_" (random-uuid))
+        opts          (merge default-opts opts)]
     (fn []
       (r/create-class
         {:component-will-unmount
-         #(reset-tooltip!)
+         #(reset-tooltip! tooltip-timer tooltip-hide-timer tooltip-state)
          :component-did-mount
          (fn [this] (reset! node (r/dom-node this)))
          :reagent-render
          (fn [component]
-           [:div {:on-mouse-enter #(start-timer! (fn []
-                                                   (swap! tooltip-state assoc :msg message)
-                                                   (set-position! @node)))
-                  :on-mouse-leave #(reset-tooltip!)}
+           [:div {:on-mouse-enter #(start-timer!
+                                    tooltip-timer
+                                    tooltip-hide-timer
+                                    (fn []
+                                      (swap! tooltip-state assoc :msg message)
+                                      (set-position! tooltip-state opts base-id @node)))
+                  :on-mouse-leave #(reset-tooltip! tooltip-timer tooltip-hide-timer tooltip-state)}
+            [tooltip-base base-id tooltip-state message]
             component])}))))
 
 (defn example-div
   [msg]
   [:div (str "I have a tooltip" msg)])
 
-(defcard tooltip-base
-  "This renders the base tooltip. It's positioned as *fixed* so that it always offsets from the
-  root page. There is only one div per tooltip and it's resued for all tips since you can only
-  have one tip open at a time."
-  (r/as-element [tooltip-base]))
-
 (defcard tooltip-example
   (r/as-element
-    [:div [with-tooltip "A helpful message." [example-div "foo"]]]))
+    [:div [with-tooltip "A helpful message." {}
+           [example-div "foo"]]]))
